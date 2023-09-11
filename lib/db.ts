55f934b850b42f64f8774b2d07968cf8b218ff44
Marycarmen <marycarmen.floreslopez@bjss.com>
2023-09-11 15:15:23 +0100

    set-up API get request

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,33 @@
 // db.js
-import { Pool } from "pg";
+//import { Pool } from "pg";
+import pg from 'pg';
+const { Client } = pg;
+const client = new Client({
+  //.Client({
+  //max: 300,
+  connectionTimeoutMillis: 10000,
+  user: 'username',
+  host: 'people-discovery.cot7tchlsltq.eu-west-2.rds.amazonaws.com',
+  database: 'posts',
+  password: 'Greg4min',
+  port: 5432,
+//  ssl: true,
+});
+client.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
+export default client;
+//let conn;
 
-let conn;
+// if (!conn) {
+//   conn = new Pool({
+//     user: 'username',
+//     password: 'Greg4min',
+//     host: 'people-discovery.cot7tchlsltq.eu-west-2.rds.amazonaws.com',
+//     port: 5432,
+//     database: 'posts',
+//   });
+// }
 
-if (!conn) {
-  conn = new Pool({
-    user: 'username',
-    password: 'Greg4min',
-    host: 'people-discovery.cot7tchlsltq.eu-west-2.rds.amazonaws.com',
-    port: 5432,
-    database: 'posts',
-  });
-}
-
-export default conn ;
+// export default conn ;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
-import Image from 'next/image';
 
-export default function Home() {
+import Image from 'next/image';
+import axios from 'axios';
+import React, { useState } from "react";
+//async function getPeopleold ():Promise<void> {
+  //console.log(value)
+  //  let data={content : value}
+  //let data = await axios.get('./api/sendpost')
+  //.then((response) => {
+    //console.log(response)
+//})
+//.catch((e) => { console.log(e)}
+//)
+// return data}
+
+
+async function getPeople () {
+  //console.log(value)
+  //  let data={content : value}
+  const data = await fetch('/api/sendpost.ts');
+  const blogData = await data.json();
+  return blogData;
+  
+}
+    
+ 
+
+export default async function Home() {
+  // let variable = getPeople();
+  // const [value, setValue] = useState<string>('');
+  
+  // setValue(variable);
+  const people = await getPeople();
   return (
     <>
       <h1>Hello Hamish!!</h1>
+      
+      <p>{people}</p>
+
     </>
   );
 }
