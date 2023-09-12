5c5d57506b86957d847d05d7c68609e63703999d
Chris Barba <chris.barba@bjss.com>
2023-09-12 12:56:24 +0100

    working db connection on main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image';
-
-export default function Home() {
+export default async function Home() {
+  const user = await import('./db/query');
+  let show = await user.default();
   return (
     <>
-      <h1>Hello Hamish!!</h1>
+      <h1>Good afternoon Hamish</h1>
+      <div>
+        {show.map((x) => (
+          <p>Good afternoon {x}</p>
+        ))}
+      </div>
     </>
   );
 }
